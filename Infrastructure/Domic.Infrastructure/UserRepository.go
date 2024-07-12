f4fcedefc18669f0ec19ba5ff76f12febaf6e4af
Hasan Karami Moheb <hasankarami2020313@gmail.com>
2024-07-12 15:12:15 +0330

    changes

--- Infrastructure/Domic.Infrastructure/UserRepository.go
+++ Infrastructure/Domic.Infrastructure/UserRepository.go
@@ -2,6 +2,7 @@ package DomicInfrastructure
 
 import (
 	"domic.domain/User/Entities"
+	"time"
 )
 
 type UserRepository struct{}
@@ -11,6 +12,18 @@ func (userRepository *UserRepository) Add(User *Entities.User) bool {
 	//something in here
 
 	return true
+
+}
+
+func (userRepository *UserRepository) AddAsync(User *Entities.User, result chan bool) {
+
+	//something in here
+
+	channel := true
+
+	time.Sleep(5 * time.Second)
+
+	result <- channel
 
 }
 
