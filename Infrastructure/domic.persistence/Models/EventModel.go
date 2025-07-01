a1350d57bba0d20549ff48f9e3a350c1744e4acc
Hasan1375-tech <karami.samdad@gmail.com>
2025-07-01 17:22:10 +0330

    update

--- Infrastructure/domic.persistence/Models/EventModel.go
+++ Infrastructure/domic.persistence/Models/EventModel.go
@@ -6,15 +6,15 @@ import (
 )
 
 type EventModel struct {
-	Id        string    `gorm:"column:Id"`
-	Name      string    `gorm:"column:Name"`
-	Service   string    `gorm:"column:Service"`
-	Table     string    `gorm:"column:Table"`
-	Action    string    `gorm:"column:Action"`
-	Payload   string    `gorm:"column:Payload"`
-	CreatedAt time.Time `gorm:"column:CreatedAt"`
-	UpdatedAt *string   `gorm:"column:UpdatedAt"`
-	IsActive  bool      `gorm:"column:IsActive"`
+	Id        string     `gorm:"column:Id"`
+	Name      string     `gorm:"column:Name"`
+	Service   string     `gorm:"column:Service"`
+	Table     string     `gorm:"column:Table"`
+	Action    string     `gorm:"column:Action"`
+	Payload   string     `gorm:"column:Payload"`
+	CreatedAt time.Time  `gorm:"column:CreatedAt"`
+	UpdatedAt *time.Time `gorm:"column:UpdatedAt"`
+	IsActive  bool       `gorm:"column:IsActive"`
 }
 
 func MapEventEntityToModel(event *Entities.Event) *EventModel {
@@ -29,7 +29,12 @@ func MapEventEntitiesToModel(events []*Entities.Event) []*EventModel {
 
 	for _, event := range events {
 
-		model := &EventModel{}
+		model := &EventModel{
+			Id:      event.GetId(),
+			Name:    event.GetName(),
+			Table:   event.GetTable(),
+			Service: event.GetService(),
+		}
 
 		models = append(models, model)
 
@@ -40,26 +45,25 @@ func MapEventEntitiesToModel(events []*Entities.Event) []*EventModel {
 }
 
 func MapEventModelToEntity(model *EventModel) *Entities.Event {
-	return Entities.Assemble(model.Id, model.FirstName, model.LastName, model.Username, model.Password,
-		model.Email, model.CreatedBy, model.CreatedRole, model.CreatedAt, model.UpdatedBy, model.UpdatedRole, model.UpdatedAt,
+	return Entities.Assemble(model.Id, model.Name, model.Service, model.Table, model.Action, model.Payload,
+		model.CreatedAt, model.UpdatedAt, model.IsActive,
 	)
 }
 
-func MapUserModelsToEntity(models []UserModel) []*Entities.Event {
+func MapEventModelsToEntity(models []EventModel) []*Entities.Event {
 
-	var users []*Entities.Event
+	var events []*Entities.Event
 
 	for _, model := range models {
 
-		userEntity := Entities.Assemble(model.Id, model.FirstName, model.LastName, model.Username, model.Password,
-			model.Email, model.CreatedBy, model.CreatedRole, model.CreatedAt, model.UpdatedBy, model.UpdatedRole,
-			model.UpdatedAt,
+		eventEntity := Entities.Assemble(model.Id, model.Name, model.Service, model.Table, model.Action, model.Payload,
+			model.CreatedAt, model.UpdatedAt, model.IsActive,
 		)
 
-		users = append(users, userEntity)
+		events = append(events, eventEntity)
 
 	}
 
-	return users
+	return events
 
 }
